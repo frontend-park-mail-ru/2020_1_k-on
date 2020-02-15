ea4edda3186efdee7bbf0cef89fd2d0ddd4d9eec
AliceSitedge <a.seledkina@mail.ru>
2020-02-15 16:44:50 +0300

    profile page inception

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/login', (req, res) => {
     res.sendFile(root + '/templates/login_page.html');
 });
 
+app.get('/profile', (req, res) => {
+    res.sendFile(root + '/templates/profile_page.html');
+});
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server listening on port ${process.env.PORT || PORT}`);
 });
