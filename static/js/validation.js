20ad1ed0fac40779f584d09bfd9b56f575fbc706
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-18 23:56:53 +0300

    update validation.js: use loop

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,30 +1,40 @@
+const inputs = {
+    login: {
+        class: 'auth-form__input_login',
+        regex: '^[a-zA-Z][a-zA-Z0-9-_.]{4,15}$',
+        error_id: 'login_error',
+        error_msg: `Логин должен содержать от 4 до 15 символов
+                     и состоять из цифр и латинских символов`,
+    },
+    password: {
+        class: 'auth-form__input_password',
+        regex: '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\\d]{4,15}$',
+        error_id: 'password_error',
+        error_msg: `Пароль должен содержать 1 заглавную букву,
+                    1 строчную букву и одну цифру,
+                    а также содержать от 4 до 15 символов`,
+    },
+};
+
 const form = document.querySelector('.auth-form');
 
-form.addEventListener('submit', evt => {
-    const login = document.querySelector('.auth-form__input_login');
-    const password = document.querySelector('.auth-form__input_password');
-
-    const login_error = document.querySelector('.auth-form__input_login ~ .auth-form__error');
-    const password_error = document.querySelector('.auth-form__input_password ~ .auth-form__error');
-
-    const login_regex = RegExp('^[a-zA-Z][a-zA-Z0-9-_.]{4,15}$');
-    const password_regex = RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\\d]{4,15}$');
-
+form.addEventListener('submit', (evt) => {
     let isValidationError = false;
 
-    if (!login_regex.test(login.value)) {
-        login_error.textContent = 'Логин должен содержать от 4 до 15 символов и состоять из цифр и латинских символов';
-        login_error.style.visibility = 'visible';
-        login.style.border = '1px solid red';
-        isValidationError = true;
-    }
+    for (const value of Object.values(inputs)) {
+        const inputItem = form.querySelector(`.${value.class}`);
+        if (inputItem == null) {
+            continue;
+        }
 
-    if (!password_regex.test(password.value)) {
-        password_error.textContent = 'Пароль должен содержать 1 заглавную букву, 1 строчную букву и одну цифру,' +
-            ' а также содержать от 4 до 15 символов';
-        password_error.style.visibility = 'visible';
-        password.style.border = '1px solid red';
-        isValidationError = true;
+        const inputRegex = RegExp(value.regex);
+        const inputError = form.querySelector(`#${value.error_id}`);
+
+        if (!inputRegex.test(inputItem.value)) {
+            inputError.textContent = value.error_msg;
+            inputError.style.visibility = 'visible';
+            isValidationError = true;
+        }
     }
 
     if (isValidationError) {
