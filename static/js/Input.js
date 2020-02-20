6d21345b821223f9dbbfa4a15312aa975b43af1c
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-21 00:12:56 +0300

    add class Input and update validation.js

--- static/js/Input.js
+++ static/js/Input.js
@@ -1,9 +1,31 @@
+const inputOptions = {
+    'auth-form__input auth-form__input_login': {
+        regex: RegExp(
+            '^[a-zA-Z][a-zA-Z0-9-_.]{4,15}$'
+        ),
+        errorID: 'login_error',
+        errorMsg: `Логин должен содержать от 4 до 15 символов
+                   и состоять из цифр и латинских символов`
+
+    },
+    'auth-form__input auth-form__input_password': {
+        regex: RegExp(
+            '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\\d]{4,15}$'
+        ),
+        errorID: 'password_error',
+        errorMsg: `Пароль должен содержать 1 заглавную букву,
+                   1 строчную букву и одну цифру,
+                   а также содержать от 4 до 15 символов`
+    },
+};
+
 class Input {
-    constructor(inputClass, regex, errorId, errorMsg) {
-        this.inputClass = inputClass;
-        this.regex = regex;
-        this.errorId = errorId;
-        this.errorMsg = errorMsg;
+    constructor(inputClass) {
+        ({
+            regex: this.regex,
+            errorID: this.errorID,
+            errorMsg: this.errorMsg
+        } = inputOptions[inputClass]);
     }
 }
 
--- static/js/validation.js
+++ static/js/validation.js
@@ -1,43 +1,20 @@
 import Input from './Input';
 
 const validation = () => {
-    const inputs = {
-        login: new Input(
-            'auth-form__input_login',
-            RegExp(
-                '^[a-zA-Z][a-zA-Z0-9-_.]{4,15}$'
-            ),
-            'login_error',
-            `Логин должен содержать от 4 до 15 символов
-                     и состоять из цифр и латинских символов`
-        ),
-        password: new Input(
-            'auth-form__input_password',
-            RegExp(
-                '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\\d]{4,15}$'
-            ),
-            'password_error',
-            `Пароль должен содержать 1 заглавную букву,
-                    1 строчную букву и одну цифру,
-                    а также содержать от 4 до 15 символов`
-        ),
-    };
-
     const form = document.querySelector('.auth-form');
 
     form.addEventListener('submit', (evt) => {
         let isValidationError = false;
 
-        for (const value of Object.values(inputs)) {
-            const inputItem = form.querySelector(`.${value.inputClass}`);
-            if (inputItem === null) {
-                continue;
-            }
+        const inputs = form.getElementsByTagName('input');
 
-            const inputError = form.querySelector(`#${value.errorId}`);
+        for (let inputItem of inputs) {
+            const inputClassItem = new Input(inputItem.className);
 
-            if (!value.regex.test(inputItem.value)) {
-                inputError.textContent = value.errorMsg;
+            const inputError = form.querySelector(`#${inputClassItem.errorID}`);
+
+            if (!inputClassItem.regex.test(inputItem.value)) {
+                inputError.textContent = inputClassItem.errorMsg;
                 inputError.style.visibility = 'visible';
                 isValidationError = true;
             }
@@ -49,4 +26,4 @@ const validation = () => {
     });
 };
 
-validation();
+export default validation;
