7099098ad625cdc620c44b47f83e1a0155648d5f
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-20 12:52:15 +0300

    add Input class for validation

---
+++ static/js/Input.js
@@ -0,0 +1,10 @@
+class Input {
+    constructor(inputClass, regex, errorId, errorMsg) {
+        this.inputClass = inputClass;
+        this.regex = regex;
+        this.errorId = errorId;
+        this.errorMsg = errorMsg;
+    }
+}
+
+export default Input;
--- static/js/validation.js
+++ static/js/validation.js
@@ -1,24 +1,26 @@
+import Input from './Input';
+
 const validation = () => {
     const inputs = {
-        login: {
-            class: 'auth-form__input_login',
-            regex: RegExp(
+        login: new Input(
+            'auth-form__input_login',
+            RegExp(
                 '^[a-zA-Z][a-zA-Z0-9-_.]{4,15}$'
             ),
-            error_id: 'login_error',
-            error_msg: `Логин должен содержать от 4 до 15 символов
-                     и состоять из цифр и латинских символов`,
-        },
-        password: {
-            class: 'auth-form__input_password',
-            regex: RegExp(
+            'login_error',
+            `Логин должен содержать от 4 до 15 символов
+                     и состоять из цифр и латинских символов`
+        ),
+        password: new Input(
+            'auth-form__input_password',
+            RegExp(
                 '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\\d]{4,15}$'
             ),
-            error_id: 'password_error',
-            error_msg: `Пароль должен содержать 1 заглавную букву,
+            'password_error',
+            `Пароль должен содержать 1 заглавную букву,
                     1 строчную букву и одну цифру,
-                    а также содержать от 4 до 15 символов`,
-        },
+                    а также содержать от 4 до 15 символов`
+        ),
     };
 
     const form = document.querySelector('.auth-form');
@@ -27,15 +29,15 @@ const validation = () => {
         let isValidationError = false;
 
         for (const value of Object.values(inputs)) {
-            const inputItem = form.querySelector(`.${value.class}`);
+            const inputItem = form.querySelector(`.${value.inputClass}`);
             if (inputItem === null) {
                 continue;
             }
 
-            const inputError = form.querySelector(`#${value.error_id}`);
+            const inputError = form.querySelector(`#${value.errorId}`);
 
             if (!value.regex.test(inputItem.value)) {
-                inputError.textContent = value.error_msg;
+                inputError.textContent = value.errorMsg;
                 inputError.style.visibility = 'visible';
                 isValidationError = true;
             }
