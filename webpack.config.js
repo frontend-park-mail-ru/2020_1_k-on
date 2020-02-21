4f8268be7b82e6b2e70bbd94c1f0630a53ec6fa3
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-21 13:57:11 +0300

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './static/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff2?|ttf|otf|eot|svg|png|jpg)$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    name: './external/[name].[ext]'
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ],
+    },
+    plugins: [],
+};
