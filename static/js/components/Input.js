de72858fc168ce0a277c8e92997e2bcaefd91c51
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-22 12:38:19 +0300

    Input.js style fix

--- static/js/components/Input.js
+++ static/js/components/Input.js
@@ -4,26 +4,26 @@ const VALIDATION_ERR_MESSAGES = {
     PASSWORD: `Пароль должен содержать 1 заглавную букву,
                1 строчную букву и одну цифру,
                а также содержать от 4 до 15 символов`,
-    EMAIL: `Неправильный формат электронной почты`,
+    EMAIL: 'Неправильный формат электронной почты',
 };
 
 const INPUTS = {
     login: {
         regex: /^[a-zA-Z][a-zA-Z0-9-_.]{4,15}$/,
         inputName: 'login',
-        errorMsg: VALIDATION_ERR_MESSAGES.LOGIN
+        errorMsg: VALIDATION_ERR_MESSAGES.LOGIN,
 
     },
     password: {
         regex: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z[0-9]{4,15}$/,
         inputName: 'password',
-        errorMsg: VALIDATION_ERR_MESSAGES.PASSWORD
+        errorMsg: VALIDATION_ERR_MESSAGES.PASSWORD,
     },
     email: {
         regex: /^\S+@\S+\.\S+$/,
         inputName: 'email',
-        errorMsg: VALIDATION_ERR_MESSAGES.EMAIL
-    }
+        errorMsg: VALIDATION_ERR_MESSAGES.EMAIL,
+    },
 };
 
 export default class Input {
@@ -31,7 +31,7 @@ export default class Input {
         ({
             regex: this.regex,
             inputName: this.inputName,
-            errorMsg: this.errorMsg
+            errorMsg: this.errorMsg,
         } = INPUTS[inputID]);
     }
 }
