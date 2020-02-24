c94b7e1049c6a8cc4ce38d68c117b8604505911b
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-24 13:08:57 +0300

    server.js: delete urls instead of /

--- server.js
+++ server.js
@@ -18,22 +18,6 @@ app.get('/', (req, res) => {
     res.sendFile(root + '/index.html');
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(root + '/templates/login_page.html');
-});
-
-app.get('/movie', (req, res) => {
-    res.sendFile(root + '/templates/movie_page.html');
-});
-
-app.get('/signup', (req, res) => {
-    res.sendFile(root + '/templates/signup_page.html');
-});
-
-app.get('/profile', (req, res) => {
-    res.sendFile(root + '/templates/profile_page.html');
-});
-
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server listening on port ${process.env.PORT || PORT}`);
 });
