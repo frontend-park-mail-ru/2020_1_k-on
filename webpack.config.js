9be77becf5534b5016a4586760353b3ef1eefda5
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-25 23:24:24 +0300

    rename index.js to app.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './static/js/index.js',
+    entry: './static/js/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
