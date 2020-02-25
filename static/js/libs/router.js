6edfd0af96077fc0df340f61b0cd32fd2b9b64c5
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-25 12:42:51 +0300

    add router

---
+++ static/js/libs/router.js
@@ -0,0 +1,47 @@
+export default class Router {
+    constructor(root) {
+        this.root = root;
+        this.routes = new Map();
+
+        this.currentRoute = null;
+
+        window.addEventListener('popstate', () => {
+            this.change(location.pathname);
+        });
+    }
+
+    add(path, root = this.root, view) {
+        this.routes.set(path, {
+            root: root,
+            view: view,
+        });
+    }
+
+    change(path) {
+        if (this.currentRoute === path) {
+            return;
+        }
+
+        if (this.routes.has(path)) {
+            this.currentRoute = path;
+            this.routes.get(path).view.render(this.root);
+            window.history.pushState(null, null, path);
+        } else {
+            console.log(`404: ${path} not found`);
+        }
+    }
+
+    start() {
+        document.addEventListener('click', (evt) => {
+            const {target} = evt;
+
+            if (target instanceof HTMLAnchorElement) {
+                evt.preventDefault();
+                this.change(target.pathname);
+            }
+        });
+
+        // начальный рендер
+        this.change(location.pathname);
+    }
+}
