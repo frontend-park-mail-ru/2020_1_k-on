9643bd081fe275c937611566832ed497a3ceadda
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-26 11:34:22 +0300

    loginView.js add removeEventListener in close method, add request on login when data validation is successful and add form error into template

--- static/js/views/loginView.js
+++ static/js/views/loginView.js
@@ -1,9 +1,14 @@
 import View from './view';
 import validation from '../libs/validation';
 
+const SUCCESS_LOGIN = 200;
+const INVALID_LOGIN = 401;
+
 const template = `
     <form class="auth-form" novalidate>
     <h1 class="auth-form__headline">ВХОД</h1>
+
+    <div class="auth-form__error" id="form_error">Error</div>
 
     <label class="auth-form__label" for="login">Логин</label>
     <input class="auth-form__input auth-form__input_login"
@@ -27,6 +32,8 @@ const template = `
 `;
 
 export default class LoginView extends View {
+    FORM_ERROR_MSG = 'Неправильный логин или пароль!';
+
     constructor() {
         super(template);
         this.validation = validation;
@@ -35,6 +42,56 @@ export default class LoginView extends View {
     render(root) {
         this.element.className = 'auth-page';
         super.render(root);
-        this.validation();
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.root.addEventListener('submit', this.onSubmit);
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        const validationResult = this.validation();
+
+        if (validationResult) {
+            const login = document.getElementById('login').value;
+            const password = document.getElementById('password').value;
+
+            fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    'username': login,
+                    'password': password,
+                },
+                ),
+            })
+                .then((res) => {
+                    if (res.status === SUCCESS_LOGIN) {
+                        this.onSuccessLogin();
+                    } else if (res.status === INVALID_LOGIN) {
+                        this.onInvalidLogin(res);
+                    }
+                })
+                .catch((error) => console.log(error));
+        }
+    }
+
+    onSuccessLogin() {
+        location.pathname = '/';
+    }
+
+    onInvalidLogin() {
+        const formError = this.element.getElementsByClassName(
+            'auth-form__error'
+        )[0];
+
+        formError.textContent = this.FORM_ERROR_MSG;
+        formError.style.visibility = 'visible';
+    }
+
+    close() {
+        this.root.removeEventListener('submit', this.onSubmit);
+        super.close();
     }
 }
