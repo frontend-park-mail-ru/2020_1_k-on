f364ffeff8bb27fb3ffa3bffc5de5854c1bee63e
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-02-27 17:04:55 +0300

    navbar.js add eventlistener on logout

--- static/js/components/navbar/navbar.js
+++ static/js/components/navbar/navbar.js
@@ -6,13 +6,29 @@ export default class Navbar {
             signup: 'Регистрация',
             login: 'Авторизация',
             profile: 'Профиль',
-            logout: 'Выйти'
+            logout: 'Выйти',
         };
         this.element = document.createElement('div');
     }
 
     render(root) {
-        this.element.innerHTML = window.fest['js/components/navbar/navbar.tmpl'](this.navbarItems);
+        this.element.innerHTML = window.fest[
+            'js/components/navbar/navbar.tmpl'
+        ](this.navbarItems);
         root.appendChild(this.element);
+
+        const logout = root.querySelector('[href="/logout"');
+        this.onLogout = this.onLogout.bind(this);
+        logout.addEventListener('click', this.onLogout);
+    }
+
+    onLogout(event) {
+        fetch('http://64.225.100.179:8080/logout', {
+            method: 'DELETE',
+            credentials: 'include',
+        })
+            .catch((error) => {
+                console.log('error happened');
+            });
     }
 }
