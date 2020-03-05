28dc6746a20ea6f0e1f9327007808f7b1ffe120b
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-03-05 23:44:34 +0300

    class view delete create element and close method delete inner html from root

--- static/js/views/view.js
+++ static/js/views/view.js
@@ -1,16 +1,15 @@
 export default class View {
     constructor(tmpl, eventBus) {
-        this.element = document.createElement('div');
         this.tmpl = tmpl;
         this.eventBus = eventBus;
     }
 
     render(root, data) {
         this.root = root;
-        root.innerHTML = this.tmpl(data);
+        this.root.innerHTML = this.tmpl(data);
     }
 
     close() {
-        this.element.innerHTML = '';
+        this.root.innerHTML = '';
     }
 }
