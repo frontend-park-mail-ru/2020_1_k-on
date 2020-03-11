a4221f13313e2c17cedac642f555fba960739113
AliceSitedge <a.seledkina@mail.ru>
2020-03-11 13:17:20 +0300

    movie page

--- static/js/views/view.js
+++ static/js/views/view.js
@@ -1,15 +1,18 @@
 export default class View {
     constructor(tmpl, eventBus) {
+        this.element = document.createElement('div');
         this.tmpl = tmpl;
         this.eventBus = eventBus;
     }
 
     render(root, data) {
         this.root = root;
-        this.root.innerHTML = this.tmpl(data);
+        this.element.innerHTML = this.tmpl(data);
+        root.innerHTML = '';
+        root.appendChild(this.element);
     }
 
     close() {
-        this.root.innerHTML = '';
+        this.element.innerHTML = '';
     }
 }
