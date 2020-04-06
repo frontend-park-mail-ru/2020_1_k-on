5893ea5c9873c75433a72df9da3e8e6699c3ef45
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-04-06 17:49:50 +0300

    add profile views

--- static/js/controllers/profileController.js
+++ static/js/controllers/profileController.js
@@ -1,15 +1,28 @@
+import ProfileSettingsView from 'views/profileSettingsView/profileSettingsView';
 import ProfileView from 'views/profileView/profileView';
 import EventBus from 'libs/eventBus';
-import {PROFILE_EVENTS} from 'libs/constants';
+import {
+    GLOBAL_EVENTS,
+    PROFILE_EVENTS,
+} from 'libs/constants';
 
 export default class ProfileController {
-    constructor(router) {
+    constructor(router, globalEventBus) {
         this.router = router;
+        this.globalEventBus = globalEventBus;
+
         this.eventBus = new EventBus();
         this.eventBus.subscribe(PROFILE_EVENTS.unauthUser, () => {
             this.router.change('/login');
         });
+        this.eventBus.subscribe(
+            PROFILE_EVENTS.logout,
+            () => {
+                this.globalEventBus.publish(GLOBAL_EVENTS.renderForUnauth);
+            },
+        );
 
+        this.settingsView = new ProfileSettingsView(this.eventBus);
         this.view = new ProfileView(this.eventBus);
     }
 }
