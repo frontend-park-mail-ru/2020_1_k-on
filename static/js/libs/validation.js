cfdcc6c44cb74cea4846b8b996aedfce5e1f9123
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-04-08 23:05:44 +0300

    validation: add param - form

--- static/js/libs/validation.js
+++ static/js/libs/validation.js
@@ -2,17 +2,22 @@ import Input from 'libs/Input';
 
 /**
  * Функция для валидации форм
+ * @param {HTMLElement} form - форма для валидации
  * @return {boolean}
  */
-const validation = () => {
-    const form = document.getElementsByClassName('auth-form')[0];
+const validation = (form = null) => {
+    // Eсли форма не передана, то делается селектор по классу и берется
+    // первый элемент
+    form = form === null ?
+        document.getElementsByClassName('auth-form')[0] :
+        form;
 
     let isValidationError = false;
 
     const inputs = form.getElementsByClassName('auth-form__input');
 
     for (const inputItem of inputs) {
-        if (inputItem.id === 'avatar') {
+        if (inputItem.id === 'repeat-password') {
             continue;
         }
         const inputClassItem = new Input(inputItem.id);
