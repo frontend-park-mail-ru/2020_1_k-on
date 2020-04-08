cb4bada0012b96de4ba6b27d05d8cadbf07412a6
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-04-09 00:42:04 +0300

    indexController.js: add event bus

--- static/js/controllers/indexController.js
+++ static/js/controllers/indexController.js
@@ -1,8 +1,21 @@
 import IndexView from 'views/indexView/indexView';
+import EventBus from 'libs/eventBus';
+import {
+    GLOBAL_EVENTS,
+    INDEX_EVENTS,
+} from 'libs/constants';
 
 export default class IndexController {
-    constructor(router) {
-        this.router = router;
-        this.view = new IndexView();
+    constructor(globalEventBus) {
+        this.globalEventBus = globalEventBus;
+        this.eventBus = new EventBus();
+        this.eventBus.subscribe(
+            INDEX_EVENTS.internalError,
+            (code) => {
+                this.globalEventBus.publish(GLOBAL_EVENTS.internalError, code);
+            }
+        );
+
+        this.view = new IndexView(this.eventBus);
     }
 }
