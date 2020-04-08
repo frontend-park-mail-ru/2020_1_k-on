4c0da9936a91eba9f05f384357062b940c54050a
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-04-08 19:35:12 +0300

    constants.js: add new constant

--- static/js/libs/constants.js
+++ static/js/libs/constants.js
@@ -2,6 +2,7 @@ export const SERVER_ADDRESS = 'http://64.225.100.179:8080';
 
 export const SUCCESS_STATUS = 200;
 export const UNAUTHORIZED_STATUS = 401;
+export const INTERNAL_ERROR_STATUS = 500;
 
 export const NAVBAR_AUTH_ITEMS = {
     profile: 'Профиль',
