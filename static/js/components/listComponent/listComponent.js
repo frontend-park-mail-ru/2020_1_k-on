195674f7fe670431cdcd97d2a0cb22fa45fa1a8e
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-04-14 16:42:25 +0300

    update list component: removed the unnecessary logic

--- static/js/components/listComponent/listComponent.js
+++ static/js/components/listComponent/listComponent.js
@@ -1,52 +1,22 @@
-import Component from 'components/component';
-import template from './listComponent.tmpl.xml';
-import Api from 'libs/api';
-import {DEFAULT_FILTERS, SUCCESS_STATUS} from 'libs/constants';
-
-export default class ListComponent extends Component {
-    constructor(type) {
-        super(template);
-        this.type = type;
-        this.chosenFilters = DEFAULT_FILTERS;
+export default class ListComponent {
+    constructor(root) {
+        this.root = root;
     }
 
-    render(root) {
-        Api.getList(this.type, {
-            maingenre: this.chosenFilters.genre.reference === '%' ?
-                this.chosenFilters.genre.reference : this.chosenFilters.genre.name,
-            year: this.chosenFilters.year.reference,
-            order: this.chosenFilters.ordering.reference,
-            page: '1',
-        })
-            .then((res) => {
-                if (res.status === SUCCESS_STATUS) {
-                    res.json().then((res) => {
-                        this.data.list = res.body;
-                        this.data.type = this.type;
-                        super.render(root);
-                    });
-                } else {
-                    console.log('something went wrong');
-                }
-            });
-    }
+    render(elements) {
+        let elem;
 
-    changeFilter(filterName, name, reference) {
-        this.chosenFilters[filterName] = {
-            name: name,
-            reference: reference,
-        };
-        this.render(this.root);
-    }
+        if (elements) {
+            elem = document.createElement('div');
+            elem.classList.add('list-component');
+            elements.forEach((item) => (elem.appendChild(item)));
+        } else {
+            elem = document.createElement('div');
+            elem.classList.add('list-component__no-results');
+            elem.innerText = 'По данному запросу ничего не найдено.';
+        }
 
-    setDefaultFilters() {
-        this.chosenFilters = Object.assign({}, DEFAULT_FILTERS);
-    }
-
-    setFilter(filterName, name, reference) {
-        this.chosenFilters[filterName] = {
-            name: name,
-            reference: reference,
-        };
+        this.root.innerHTML = '';
+        this.root.appendChild(elem);
     }
 }
