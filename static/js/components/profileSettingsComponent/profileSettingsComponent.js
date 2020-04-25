a46c4b9666485f9e3e3833f1374a68a5fc734a24
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-04-25 16:44:13 +0300

    profileSettingsComponent: update class naming

--- static/js/components/profileSettingsComponent/profileSettingsComponent.js
+++ static/js/components/profileSettingsComponent/profileSettingsComponent.js
@@ -48,7 +48,7 @@ export default class ProfileSettingsComponent extends Component {
         this.element.getElementsByClassName('user-settings__change-password')[0]
             .addEventListener('click', this.openModal.bind(this));
         this.modalWrapper = this.element.getElementsByClassName('modal-wrapper')[0];
-        this.element.getElementsByClassName('modal-close')[0]
+        this.element.getElementsByClassName('modal-form__close')[0]
             .addEventListener('click', this.closeModal.bind(this));
     }
 
@@ -60,7 +60,7 @@ export default class ProfileSettingsComponent extends Component {
     closeModal() {
         this.passwordForm.clearForm();
         this.element.getElementsByClassName('modal-password')[0]
-            .classList.remove('modal-password_error');
+            .classList.remove('modal-form_error');
         this.element.getElementsByClassName('modal-error')[0].style.opacity = '0';
         this.modalWrapper.style.visibility = 'hidden';
         document.body.style.overflow = 'auto';
@@ -73,7 +73,7 @@ export default class ProfileSettingsComponent extends Component {
         })
             .then((res) => {
                 if (res.status === SUCCESS_STATUS) {
-                    this.username = inputsValue.login;
+                    this.login = inputsValue.login;
                     this.email = inputsValue.email;
                     this.eventBus.publish(PROFILE_EVENTS.showMsg, PROFILE_MSGS.settings_update);
                     this.eventBus.publish(
@@ -84,13 +84,12 @@ export default class ProfileSettingsComponent extends Component {
                 } else if (res.status === INTERNAL_ERROR_STATUS) {
                     return Promise.reject(res);
                 } else {
-                    this.dataForm.setInputValue('login', this.login);
-                    this.dataForm.setInputValue('email', this.email);
+                    this.settingsForm.setInputValue('login', this.login);
+                    this.settingsForm.setInputValue('email', this.email);
                     this.eventBus.publish(PROFILE_EVENTS.showMsg, PROFILE_MSGS.user_exists, true);
                 }
             })
             .catch((err) => {
-                console.log(err);
                 this.eventBus.publish(PROFILE_EVENTS.internalError, err.status);
             });
     }
@@ -98,7 +97,7 @@ export default class ProfileSettingsComponent extends Component {
     onPasswordSubmit(inputsValue) {
         if (inputsValue.password !== inputsValue['repeat-password']) {
             this.element.getElementsByClassName('modal-password')[0]
-                .classList.add('modal-password_error');
+                .classList.add('modal-form_error');
             const modalError = this.element.getElementsByClassName('modal-error')[0];
             modalError.style.opacity = '1';
             modalError.innerHTML = PROFILE_MSGS.passwords_not_match;
