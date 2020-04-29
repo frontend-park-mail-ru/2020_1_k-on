cc1c77694e7cf0b29bc94d2c0173143c876aa560
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-04-29 22:10:43 +0300

    constants.js: add CONFLICT_STATUS

--- static/js/libs/constants.js
+++ static/js/libs/constants.js
@@ -5,6 +5,7 @@ export const BAD_REQUEST_STATUS = 400;
 export const UNAUTHORIZED_STATUS = 401;
 export const FORBIDDEN_STATUS = 403;
 export const NOT_FOUND_STATUS = 404;
+export const CONFLICT_STATUS = 409;
 export const INTERNAL_ERROR_STATUS = 500;
 
 export const INTERNAL_ERROR_MSG = 'Произошла внутренняя ошибка сервера';
