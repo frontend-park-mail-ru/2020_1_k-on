4cc3b01bb1afbf9fddd29a753db047911afc8a4b
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-05-11 13:48:27 +0300

    add clean-webpack-plugin for clear dist folder from old bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const autoprefixer = require('autoprefixer');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackGitHash = require('webpack-git-hash');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const githashLength = 24;
 
 module.exports = {
@@ -75,5 +76,6 @@ module.exports = {
         new WebpackGitHash({
             hashLength: githashLength,
         }),
+        new CleanWebpackPlugin(),
     ],
 };
