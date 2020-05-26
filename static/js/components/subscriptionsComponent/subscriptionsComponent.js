4f1aacc4b068e96d6e918c37e63b4d06b4766a0a
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-05-26 13:56:45 +0300

    add msg on profile page if subscriptions is empty

--- static/js/components/subscriptionsComponent/subscriptionsComponent.js
+++ static/js/components/subscriptionsComponent/subscriptionsComponent.js
@@ -18,6 +18,18 @@ export default class SubscriptionsComponent extends Component {
     }
 
     afterRender() {
+        if (!this.subsList.length) {
+            const msgElem = document.createElement('div');
+            msgElem.classList.add('subscriptions-empty-msg');
+            msgElem.innerText = 'Вы еще не подписаны ни на один плейлист! Подписаться можно на ';
+            const linkToIndex = document.createElement('a');
+            linkToIndex.classList.add('subscriptions-empty-msg__link');
+            linkToIndex.href = '/';
+            linkToIndex.innerText = 'главной странице.';
+            msgElem.appendChild(linkToIndex);
+            this.element.appendChild(msgElem);
+        }
+
         this.subsList.forEach((subItem) => {
             subItem.films = subItem.films === null ? [] : subItem.films;
             subItem.series = subItem.series === null ? [] : subItem.series;
